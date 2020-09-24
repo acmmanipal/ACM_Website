f83e87395286dc45bc9a746709dbc94f890cd641
unknown <leander.maben@gmail.com>
2020-09-24 20:13:32 +0530

    - Added client
    - Configured Express Server to serve React

--- app.js
+++ app.js
@@ -18,10 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-if(process.env.NODE_ENV==='production'){
-  app.use(express.static('client/build'));
-}
+app.use(express.static('client/build'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path=require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/*', function(req, res, next) {
+  res.sendFile(path.join(__dirname,'..','client','build','index.html'));
 });
 
 module.exports = router;
