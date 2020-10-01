c207f665e98aa050e8b6f4806e7cdb39e16c93e4
Sohom <sohom.datta@learner.manipal.edu>
2020-10-01 23:06:24 +0530

    Split the board member cards into another component
    Added a new component, BoardMemberCard and created a
    json file, board-members.json to load the board members
    data from. Also added dummy links.

---
+++ client/src/components/BoardMemberCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function BoardMemberCard (props) {
+    return (<div className="card carousel-item">
+    <div className="card-image ">
+        <img className="responsive-img" src={props.image}/>
+        <span className="card-title">{props.name}</span>
+    </div>
+
+    <div className="card-content">
+    <h4>{props.position}</h4>
+        <p>{props.blurb}
+        </p>
+        <br/>
+        <p className="board-profile-icons-container">
+            <span className="board-profile-icon">
+                <a href={props.social_media_urls.linkedin} ><i className="fab fa-2x fa-linkedin"></i></a>
+            </span>
+            <span className="board-profile-icon">
+                <a href={props.social_media_urls.github} ><i className="fab fa-2x fa-github"> </i></a>
+            </span>
+            <span className="board-profile-icon">
+                <a href={props.social_media_urls.github} ><i className="fab fa-2x fa-medium"></i></a>
+            </span>
+        </p>
+    </div>
+</div>);
+}
+
+export default BoardMemberCard;
