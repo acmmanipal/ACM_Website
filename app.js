14128514483024e9912e7c335c9f9053d33d6e21
Sohom <sohom.datta@learner.manipal.edu>
2020-10-06 23:09:28 +0530

    Made /users endpoint reachable via the web interface
    Bug: #15

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('client/build'));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+app.use('/*', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
