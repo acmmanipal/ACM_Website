e70c2c0a4cffb9c0f1c082f69644994c6eeeac0c
unknown <leander.maben@gmail.com>
2020-10-13 23:32:30 +0530

    Added session support

--- app.js
+++ app.js
@@ -3,9 +3,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config');
+
+const cors=require('cors');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const connect = mongoose.connect(config.MongoUrl,{useNewUrlParser:true});
+
+connect.then((db)=>{
+  console.log("Connected correctly to the server.")
+}),(err)=>{console.log(err);}
+
+const sessionStore = new MongoStore({
+  mongooseConnection:mongoose.connection,
+  collection:'sessions'
+});
+
+const sessionMiddleWare = session({
+  name:'session',
+  secret:config.sessionSecret,
+  resave:false,
+  saveUninitialized:true,
+  store:sessionStore,
+  cookie:{
+    maxAge:1000*60*60*24
+  }
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const passport = require('passport');
 
 var app = express();
 
@@ -19,7 +48,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('client/build'));
-//app.use('/static', express.static(path.join(__dirname, '../client/build//static')));
+
+const corsOptions= {
+  origin: true,
+  credentials: true
+};
+
+app.options('*', cors(corsOptions));
+
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Credentials','true');
+  next();
+});
+
+app.use((req,res,next)=>sessionMiddleWare(req,res,next));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/api/users', usersRouter);
 app.use('/*', indexRouter);
 
